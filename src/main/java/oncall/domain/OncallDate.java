a5601c8c81f0462c0718a15742df273bfa1a3271
PgmJun <chltmdwns96@gmail.com>
2023-12-16 17:21:51 +0900

    refactor: 매직넘버, 매직리터럴 제거

--- src/main/java/oncall/domain/OncallDate.java
+++ src/main/java/oncall/domain/OncallDate.java
@@ -12,6 +12,11 @@ public class OncallDate {
     private final static List<Integer> dates = List.of(0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
     private final static List<List<Integer>> holyDays = List.of(List.of(), List.of(1), List.of(), List.of(1), List.of(),
             List.of(5), List.of(6), List.of(), List.of(15), List.of(), List.of(3, 9), List.of(), List.of(25));
+    private final static int MIN_MONTH = 1;
+    private final static int MAX_MONTH = 12;
+    private final static int FRIDAY_INDEX = 4;
+    private final static int WEEK_SIZE = 7;
+
 
     public OncallDate(OncallDateDto dto) {
         validate(dto);
@@ -35,7 +40,7 @@ public class OncallDate {
     }
 
     private static void validateMonth(OncallDateDto dto) {
-        if (dto.getMonth() < 1 || dto.getMonth() > 12) {
+        if (dto.getMonth() < MIN_MONTH || dto.getMonth() > MAX_MONTH) {
             throw new IllegalArgumentException("월은 1~12월만 입력할 수 있습니다.");
         }
     }
@@ -50,7 +55,7 @@ public class OncallDate {
 
     public boolean isHolyDayOrWeekend(int month, int date) {
         int dayOfWeekIndex = calculateDayOfWeekIndex(date);
-        if (holyDays.get(month).contains(date) || (dayOfWeekIndex > 4)) {
+        if (holyDays.get(month).contains(date) || (dayOfWeekIndex > FRIDAY_INDEX)) {
             return true;
         }
         return false;
@@ -61,6 +66,6 @@ public class OncallDate {
     }
 
     public int calculateDayOfWeekIndex(int date) {
-        return (dayOfWeeks.indexOf(dayOfWeek) + (date - 1)) % 7;
+        return (dayOfWeeks.indexOf(dayOfWeek) + (date - 1)) % WEEK_SIZE;
     }
 }
--- src/main/java/oncall/domain/Worker.java
+++ src/main/java/oncall/domain/Worker.java
@@ -3,13 +3,16 @@ package oncall.domain;
 public class Worker {
     private final String name;
 
+    private final static int MIN_NAME_LENGTH = 1;
+    private final static int MAX_NAME_LENGTH = 5;
+
     public Worker(String name) {
         validate(name);
         this.name = name;
     }
 
     private void validate(String name) {
-        if (name.isBlank() || name.length() < 1 || name.length() > 5) {
+        if (name.isBlank() || name.length() < MIN_NAME_LENGTH || name.length() > MAX_NAME_LENGTH) {
             throw new IllegalArgumentException("근무자명은 1자 이상 5자 이하여야 합니다");
         }
     }
